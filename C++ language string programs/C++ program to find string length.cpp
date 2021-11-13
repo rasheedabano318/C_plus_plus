2ab36635d5771d62993f06b84866917a665fc4c9
Rasheeda Bano <93844860+rasheedabano318@users.noreply.github.com>
2021-11-13 17:20:01 +0500

    Create C++ program to find string length.cpp

---
+++ C++ language string programs/C++ program to find string length.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+    string str = "Tech Study";
+ 
+    // you can also use str.length()
+    cout << "String Length = " << str.size();
+ 
+    return 0;
+}
+ 
