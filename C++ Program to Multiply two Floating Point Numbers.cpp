55c25d6d296f943526262e973597220f78dcbb3e
Rasheeda Bano <93844860+rasheedabano318@users.noreply.github.com>
2021-11-13 16:59:06 +0500

    Create C++ Program to Multiply two Floating Point Numbers.cpp

---
+++ C++ Program to Multiply two Floating Point Numbers.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;	
+
+int main() 
+{
+	double num1;
+	double num2;
+	double total;
+	cout << "Enter first number :";
+	cin >> num1;
+	
+	cout << "Enter second number :";
+	cin >> num2;
+		
+	total = num2 * num1;
+	cout  << "Total is :" << total;
+	
+	return 0;
+}
