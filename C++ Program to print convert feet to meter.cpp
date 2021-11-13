45a42514f740f8257c7a2473182048d21b59c209
Rasheeda Bano <93844860+rasheedabano318@users.noreply.github.com>
2021-11-13 17:01:08 +0500

    Create C++ Program to print convert feet to meter.cpp

---
+++ C++ Program to print convert feet to meter.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;	
+
+int main() 
+{
+	float meter,feet;
+
+	cout<< "Enter feet : ";
+    cin >> feet ;    
+
+    meter = feet / 3.2808399;
+    cout<< meter ;
+
+    return 0;
+}
